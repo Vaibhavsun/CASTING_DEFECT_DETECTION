e6a09d80d9e4beaccbfa4eb73a0aa4102088cdcf
Vaibhavsun <vaibhav@dbchat.in>
2025-06-06 10:23:58 +0530

    Adding Files

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from model_predict import prediction
+
+st.title("Casting Defect Detection")
+label=None
+casting_image=st.file_uploader("Upload an image", type=["jpg", "jpeg"])
+if casting_image is not None:
+    st.image(casting_image, caption="Uploaded Image", use_container_width=True)
+    st.write("Image uploaded successfully!")
+    with open('casting_image.jpg', 'wb') as f:
+        f.write(casting_image.getbuffer())
+    st.write("Label":prediction("casting_image.jpg"))
+
+    
+
+
+
+    
---
+++ model_predict.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tf.keras.models import load_model
+import streamlit as st
+
+@st.cache_resource
+def model_loading():
+    model=load_model('casting_detection.h5')
+    return model
+
+
+def load_image(path):
+    image = tf.io.read_file(path)
+    image = tf.image.decode_jpeg(image, channels = 3)
+    image = tf.image.resize(image, size=(256, 256))
+    image = tf.cast(image, dtype= tf.float32)/255
+    return image
+
+
+def prediction(path):
+
+    img=load_image(path)
+    model=model_loading()
+    img=tf.expand_dims(img,axis=0)
+    pred=model(img)
+    print(pred[0][0])
+    if int(pred[0][0])==0:
+        return "DEFECT"
+    else:
+        return "NO DEFECT"
+
+
